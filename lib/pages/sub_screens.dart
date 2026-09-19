2f4795a538b87c6adaac8ee7c22b6f636550b842
farhadrasa78-a11y <farhadrasa78@gmail.com>
2026-09-19 20:55:03 +0330

    Update safirBrandColor to new hex value

--- lib/pages/sub_screens.dart
+++ lib/pages/sub_screens.dart
@@ -2,7 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-final Color safirBrandColor = const Color(0xFF145A41);
+final Color safirBrandColor = const Color(0xFF1B7A57);
 final Color safirAccentColor = const Color(0xFF22C55E);
 
 // ----------------------------------------------------
