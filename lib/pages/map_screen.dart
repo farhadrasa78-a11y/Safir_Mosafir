150304ebfaea814900b076cf247e83d712dff00f
farhadrasa78-a11y <farhadrasa78@gmail.com>
2026-09-25 00:06:56 +0330

    Remove iconIgnorePlacement from driver symbol options
    Remove iconIgnorePlacement from driver symbol options.

--- lib/pages/map_screen.dart
+++ lib/pages/map_screen.dart
@@ -307,7 +307,6 @@ bool _hasPlayedArrivedSound = false;
       iconImage: 'driver-car-icon',
       iconAnchor: 'center',
       iconRotate: heading,
-      iconIgnorePlacement: true,
     );
 
     if (_driverLiveSymbol == null) {
